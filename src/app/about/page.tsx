ddf47eea01c4b766c5a50223e11bb6df22778522
Sengicarlos <sengicarloscharles@gmail.com>
2025-11-12 04:10:06 +0300

    Lean about static and dynamic routing and not to use metadata in the ssr /app/counter and app/products/productid

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,10 @@
+//working with metadata in nextjs
+export const metadata = {
+    title: "About Page",
+    description: "This is the about page description",
+}
+
+
 export default function about(){
     return <h1>About Page</h1>
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,21 @@
-export default async function productDetails({params,}:{
+import {Metadata} from "next"
+
+type Props = {
     params: Promise<{productId: string}>;
-}){
+}
+
+export const generateMetadata = async ({params}: Props) => {
+    const id = (await params).productId;
+    const title = await new Promise((resolve) => {
+        setTimeout(()=>{
+            resolve(`iphone ${id}`)
+        })})
+    return{
+        title:`Product ${title}`,
+    }
+}
+
+export default async function productDetails({params}: Props) {
     const productId = (await params).productId;
     return <h1>Details about product {productId}</h1>
 }
