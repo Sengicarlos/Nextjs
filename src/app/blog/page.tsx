47c5f38d98d160d31127e812116b67b766932e21
Sengicarlos <sengicarloscharles@gmail.com>
2025-11-12 04:30:58 +0300

    leant about title metadata object which are defauld,templete and absolute app/blog app/layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,11 @@
 // This is how to handle routing in nextjs 
+
+//this is metadata for blog paga
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog"
+    }
+}
 export default function blog(){
     return <h1>Blog Page</h1>
 }
